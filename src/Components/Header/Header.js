2d8498a0600558dddf549259397775dc49ac2c3a
acidsafari <samuelklettnavarro@gmail.com>
2024-03-29 18:37:26 +1100

    embeded player with react-youtube plus styles

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "./Header.css";
+import HeaderVideo from './HeaderVideo';
 
-export default function Header() {
+function Header(youtubeId) {
   return (
     <div className="Header">
-        <h2>Creative Demonstration app</h2>
-    </div>
-  );
-} 
+      <div className="HeaderTextCenterrow">
+        <h1>Be Part of Something Bigger !</h1>
+      </div>
+      <HeaderVideo />
+    </div>    
+  )
+}
+/*
+import YouTube from 'react-youtube';
+
+const youtubeId = "TOHaHEmRtKg"
+
+function Header(youtubeId) {
+  return (
+    <div className="Header">
+      <div className="HeaderTextCenterrow">
+        <h1>Be Part of Something Bigger !</h1>
+      </div>
+      <div className={youtubeId ? "ctn-youtube": null}>
+        {youtubeId && <YouTube videoId={youtubeId} className='youtube'/>}
+      </div>
+    </div>    
+  )
+}
+*/
+export default Header;
