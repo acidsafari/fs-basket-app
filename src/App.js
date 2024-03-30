48ebcf4c3c3780e55de474d3db85c73ab02106c4
acidsafari <samuelklettnavarro@gmail.com>
2024-03-30 19:58:11 +1100

    initial subscribe commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from "./Components/Navigation/Navigation";
 import Header from "./Components/Header/Header";
 import News from "./Components/News/News";
 import Subscribe from "./Components/Subscribe/Subscribe";
+import SubscribeTest from "./Components/Subscribe/SubscribeTest";
 import Main from "./Components/Main/Main";
 import ResourceHub from "./Components/ResourceHub/ResourceHub";
 import Footer from "./Components/Footer/Footer";
@@ -15,7 +16,7 @@ function App() {
       <Header />
       <div className='MainAside'>
         <News />
-        <Subscribe />
+        <SubscribeTest />
       </div>
       <Main />
       <ResourceHub />
